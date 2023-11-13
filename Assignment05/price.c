60fc2e5df11bd9c97363a2d35917ccb1c4616a27
DoguMac <dogu2205@icloud.com>
2023-11-13 13:17:53 +0100

    prog 1 Assigment 5

---
+++ Assignment05/price.c
@@ -0,0 +1,19 @@
+/*
+ * Compile:         make X
+ * Run:             ./X
+ * Compile and Run: make X && ./X
+ */
+#include "base.h" // prog1lib
+
+// todo: define constants
+
+// todo: write function price
+
+// todo: write function price_test
+// test returned values of price using test_equal_i
+
+int main(void) {
+    // todo: call price_test function
+    printf("test\n");
+    return 0;
+}
