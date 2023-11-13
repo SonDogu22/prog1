5a69fa36d9dbbc5770a1fa371ecb87f5b08fedd1
Doguhan Davut <dogu2205@icloud.com>
2023-11-13 18:00:12 +0100

    aufgabe price ist done

--- Assignment05/price.c
+++ Assignment05/price.c
@@ -6,14 +6,50 @@
 #include "base.h" // prog1lib
 
 // todo: define constants
-
+    int standardPrice = 100;
+    int priceReduction1 = 90;
+    int priceReduction2 = 75;
+    int noShippingCost = 2000;
+    int shippingCost = 1999;
+    int costForShipping = 500;
+    int noCost = 0;
 // todo: write function price
+int price(int product){
 
+    int cost = 0;
+
+    if(product <= 0){
+		return 0;
+	}else if (product < 10){
+        cost = product * standardPrice;
+    }else if (product < 50 && product >= 10){
+        cost = product * priceReduction1;
+    }else if (product >= 50){
+        cost = product * priceReduction2;
+    } 
+    
+    
+    if (cost < shippingCost)
+    {
+        cost += costForShipping;
+    }
+
+    return cost;
+}
 // todo: write function price_test
-// test returned values of price using test_equal_i
+void price_test (void) {
+	test_equal_i(price(0), 0);
+	test_equal_i(price(9), 1400);
+	test_equal_i(price(15), 1850);
+	test_equal_i(price(23), 2070);
+	test_equal_i(price(50), 3750);
+	test_equal_i(price(-1), 0);
+}
+
 
 int main(void) {
     // todo: call price_test function
-    printf("test\n");
+    price_test();
     return 0;
+
 }
