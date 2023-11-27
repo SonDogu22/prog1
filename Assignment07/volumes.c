ec1450c45edf74ebec6b5291d88b719e22a3ac56
Dogu Laptop <dogu2205@icloud.com>
2023-11-27 15:53:19 +0100

    Volumes done

--- Assignment07/volumes.c
+++ Assignment07/volumes.c
@@ -24,22 +24,66 @@ typedef struct sphere_s {
 
 typedef struct body_s {
     // todo
+    TypeTag tag;
+    struct cylinder_s Cylinder;
+    struct cuboid_s Cuboid;
+    struct sphere_s Sphere;
 } Body;
+
+/*
+TypTag:
+TypeTag wird verwendet, um zwischen verschiedenen Formen zu unterscheiden. 
+Da die Tags Aufzählungen sind, kann der Compiler mithilfe der Switch-Anweisungen überprüfen,
+welcher Fall der Aufzählung verwendet wird. In C sind Listen ganzzahlige Werte. 
+Die Konstruktorfunktionen make_cylinder, make_cuboid und make_sphere werden verwendet, 
+um Variablen für den jeweiligen Tag zu erstellen.
+*/
 
 Body make_cylinder(double r, double h) {
     // todo
+    require("r and h must be a positive value", r > 0 && h > 0);
+    Body body;
+    body.tag = CYLINDER;
+    body.Cylinder.r = r;
+    body.Cylinder.h = h;
+    return body;
 }
 
 Body make_cuboid(double a, double b, double c) {
     // todo
+    require("a, b and c must be a positive value", a > 0 && b > 0 && c > 0 );
+    Body body;
+    body.tag = CUBOID;
+    body.Cuboid.a = a;
+    body.Cuboid.b = b;
+    body.Cuboid.c = c;
+    return body;
 }
 
 Body make_sphere(double r) {
     // todo
+    require("r must be a positive value", r > 0);
+    Body body;
+    body.tag = SPHERE;
+    body.Sphere.r = r;
+    return body;
 }
 
 double volume(Body body) {
-    // todo
+    switch (body.tag)
+    {
+    case SPHERE:
+        return 4/3.0*M_PI*(body.Sphere.r*body.Sphere.r*body.Sphere.r);
+        break;
+    
+    case CUBOID:
+        return body.Cuboid.a*body.Cuboid.b*body.Cuboid.c;
+        break;
+    case CYLINDER:
+        return M_PI*(body.Cylinder.r*body.Cylinder.r)*body.Cylinder.h;
+        break;
+    }
+        
     return -1;
 }
 
